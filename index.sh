0a2551a1577f38f990f00da0b617382296df38e8
zhongyu.pzy <zhongyu.pzy@alibaba-inc.com>
2019-10-21 14:19:11 +0800

    modify script, omitting README in path

--- index.sh
+++ index.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+sed -i "" -e '/## Index/,$d' ./README.md
+echo "## Index" >> ./README.md
+echo "" >> ./README.md
+
 mds=$(find . -name "README.md" | xargs ls -1t)
 
 for md in $mds; do
-    head -n1 $md | grep -q "-" && echo "- ["$(head -n1 $md | sed 's/# //g')"]("$md")" 
+    head -n1 $md | grep -q "-" && echo "- ["$(head -n1 $md | sed 's/# //g')"]("$(echo $md | sed 's/\/README.md//g')")" >> ./README.md
 done
