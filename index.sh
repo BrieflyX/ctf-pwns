767a0cfa7178db7dc2b88a91dff4ba9ebdb209dd
zhongyu.pzy <zhongyu.pzy@alibaba-inc.com>
2019-10-22 10:53:10 +0800

    sort by ctime

--- index.sh
+++ index.sh
@@ -4,7 +4,7 @@ sed -i -e '/## Index/,$d' ./README.md
 echo "## Index" >> ./README.md
 echo "" >> ./README.md
 
-mds=$(find . -name "README.md" | xargs ls -1t)
+mds=$(find . -name "README.md" | xargs ls -c -1t)
 
 for md in $mds; do
     head -n1 $md | grep -q "-" && echo "- ["$(head -n1 $md | sed 's/# //g')"]("$(echo $md | sed 's/\/README.md//g')")" >> ./README.md
